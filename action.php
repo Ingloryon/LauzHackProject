b531f2be8b959816d7f0d60045a730aff3fd779a
ChapatteMateo <mateo.chapatte@epfl.ch>
2019-11-17 08:13:24 +0100

    update action.php

--- action.php
+++ action.php
@@ -1,20 +1,20 @@
 <?php
 if( $_POST )
 {
-  $con = mysql_connect("localhost","Mateo","password");
+  //$con = mysql_connect('localhost','ToBeRemoved','');
+  $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','ToBeRemoved','');
 
-  if (!$con)
+  /*if (!$con)
   {
     die('Could not connect: ' . mysql_error());
-  }
+  }*/
 
-  mysql_select_db("test", $con);
 
   $users_prenom = $_POST['Prenom'];
   $users_nom = $_POST['Nom'];
   $users_mail = $_POST['Mail'];
   $users_password = $_POST['Password'];
-  $users_ID = $_POST['ID'];
+  $users_ID = 69420;
 
   $users_prenom = mysql_real_escape_string($users_prenom);
   $users_nom = mysql_real_escape_string($users_nom);
@@ -22,15 +22,14 @@ if( $_POST )
   $users_password = mysql_real_escape_string($users_password);
   $users_ID = mysql_real_escape_string($users_ID);
 
-  $query = "
-  INSERT INTO `test`.`users` (`Prenom`, `Nom`, `Mail`, `Password`,
-        `ID`) VALUES ('$users_prenom', '$users_nom',
-        '$users_mail', '$users_password', '$users_ID');";
+  $query = '
+  INSERT INTO `users` (`Prenom`, `Nom`, `Mail`, `Password`,
+        `ID`) VALUES ($users_prenom, $users_nom,
+        $users_mail, $users_password, $users_ID)';
 
-  mysql_query($query);
+
+  $bdd -> exec($query);
 
   echo "<h2>Thank you for your Comment!</h2>";
-
-mysql_close($con);
 }
 ?>
